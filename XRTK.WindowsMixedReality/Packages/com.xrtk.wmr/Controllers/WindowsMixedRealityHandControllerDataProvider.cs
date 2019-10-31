14d72bfcc8523ed8837dc7c9bf036f88f752224d
Dino Fejzagic <dino.f@live.de>
2019-10-31 17:33:23 +0100

    Remove need for hand controller data provider profile

--- XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Controllers/WindowsMixedRealityHandControllerDataProvider.cs
+++ XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Controllers/WindowsMixedRealityHandControllerDataProvider.cs
@@ -1,14 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using XRTK.Definitions.Controllers.Hands;
+using XRTK.Definitions.Controllers;
 using XRTK.Providers.Controllers.Hands;
 
 namespace XRTK.WindowsMixedReality.Controllers
 {
-    public class WindowsMixedRealityHandControllerDataProvider : BaseHandControllerDataProvider
+    public class WindowsMixedRealityHandControllerDataProvider : BaseHandControllerDataProvider<WindowsMixedRealityHandController>
     {
-        public WindowsMixedRealityHandControllerDataProvider(string name, uint priority, HandControllerDataProviderProfile profile)
+        public WindowsMixedRealityHandControllerDataProvider(string name, uint priority, BaseMixedRealityControllerDataProviderProfile profile)
             : base(name, priority, profile) { }
     }
 }
