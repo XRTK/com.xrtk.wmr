33f61c54fba6ecbd8edee36253811bf28970c065
Stephen Hodgson <StephenHodgson@users.noreply.github.com>
2019-12-17 20:32:22 -0500

    updated inspector namespace (#47)
    * updated inspector namespace
    
    * updated package version and updated dependencies
    
    * updated package dependencies

--- XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Inspectors/WindowsMixedRealityMotionControllerMappingProfileInspector.cs
+++ XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Inspectors/WindowsMixedRealityMotionControllerMappingProfileInspector.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.﻿
 
 using UnityEditor;
-using XRTK.Inspectors.Profiles;
+using XRTK.Inspectors.Profiles.InputSystem.Controllers;
 using XRTK.WindowsMixedReality.Profiles;
 
 namespace XRTK.WindowsMixedReality.Inspectors
