254ad3a9274ed40e545fa2e3a19c1eea18d80019
Dino Fejzagic <dino.f@live.de>
2020-01-10 21:02:28 +0100

    Extend wmr controller interface

---
+++ XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Interfaces/Providers/Controllers/IWindowsMixedRealityController.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Interfaces.Providers.Controllers;
+
+#if UNITY_WSA
+using UnityEngine.XR.WSA.Input;
+#endif
+
+namespace XRTK.WindowsMixedReality.Interfaces.Providers.Controllers
+{
+    /// <summary>
+    /// Windows Mixed Reality extensions to the core <see cref="IMixedRealityController"/> interface.
+    /// </summary>
+    public interface IWindowsMixedRealityController : IMixedRealityController
+    {
+#if UNITY_WSA
+
+        /// <summary>
+        /// Update the controller data from the provided platform state.
+        /// </summary>
+        /// <param name="interactionSourceState">The InteractionSourceState retrieved from the platform.</param>
+        void UpdateController(InteractionSourceState interactionSourceState);
+
+#endif
+    }
+}
