d35e73876a09d84ad44a0523e5eb2d60bd791180
Dino Fejzagic <dino.f@live.de>
2020-02-15 14:02:19 +0100

    Introduce shared hand profile configuraiton

--- XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Controllers/Hands/WindowsMixedRealityHandControllerDataProvider.cs
+++ XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Controllers/Hands/WindowsMixedRealityHandControllerDataProvider.cs
@@ -2,9 +2,9 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using XRTK.Providers.Controllers;
-using XRTK.WindowsMixedReality.Profiles;
 using XRTK.Providers.Controllers.Hands;
 using System.Collections.Generic;
+using XRTK.Definitions.Controllers;
 
 #if WINDOWS_UWP
 using XRTK.WindowsMixedReality.Extensions;
@@ -33,13 +33,10 @@ namespace XRTK.WindowsMixedReality.Controllers.Hands
         /// <param name="name">Name of the data provider as assigned in the configuration profile.</param>
         /// <param name="priority">Data provider priority controls the order in the service registry.</param>
         /// <param name="profile">Controller data provider profile assigned to the provider instance in the configuration inspector.</param>
-        public WindowsMixedRealityHandControllerDataProvider(string name, uint priority, WindowsMixedRealityHandControllerDataProviderProfile profile)
+        public WindowsMixedRealityHandControllerDataProvider(string name, uint priority, BaseMixedRealityControllerDataProviderProfile profile)
             : base(name, priority, profile)
-        {
-            this.profile = profile;
-        }
+        { }
 
-        private readonly WindowsMixedRealityHandControllerDataProviderProfile profile;
         private readonly Dictionary<uint, MixedRealityHandController> activeControllers = new Dictionary<uint, MixedRealityHandController>();
         private readonly WindowsMixedRealityHandDataConverter handDataConverter = new WindowsMixedRealityHandDataConverter();
 
