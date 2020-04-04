673604268d368b8162d5ce3a58295c7f651fd937
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-04 19:55:02 -0400

    use BaseHandDataProviderProfile

--- XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Profiles/WindowsMixedRealityHandControllerDataProviderProfile.cs
+++ XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Profiles/WindowsMixedRealityHandControllerDataProviderProfile.cs
@@ -3,56 +3,12 @@
 
 using UnityEngine;
 using XRTK.Definitions.Utilities;
-using XRTK.Definitions.Controllers;
 using XRTK.Definitions.Controllers.Hands;
 
 namespace XRTK.WindowsMixedReality.Profiles
 {
     [CreateAssetMenu(menuName = "Mixed Reality Toolkit/Input System/Controller Data Providers/Windows Mixed Reality Hand", fileName = "WindowsMixedRealityHandControllerDataProviderProfile", order = (int)CreateProfileMenuItemIndices.Input)]
-    public class WindowsMixedRealityHandControllerDataProviderProfile : BaseMixedRealityControllerDataProviderProfile
+    public class WindowsMixedRealityHandControllerDataProviderProfile : BaseHandDataProviderProfile
     {
-        #region Global Hand Settings Overrides
-
-        [Header("General Settings")]
-
-        [SerializeField]
-        [Tooltip("If set, hand mesh data will be read and available for visualization. Disable for optimized performance.")]
-        private bool handMeshingEnabled = false;
-
-        /// <summary>
-        /// If set, hand mesh data will be read and available for visualization. Disable for optimized performance.
-        /// </summary>
-        public bool HandMeshingEnabled => handMeshingEnabled;
-
-        [Header("Hand Physics")]
-
-        [SerializeField]
-        [Tooltip("If set, hands will be setup with colliders and a rigidbody to work with Unity's physics system.")]
-        private bool handPhysicsEnabled = false;
-
-        /// <summary>
-        /// If set, hands will be setup with colliders and a rigidbody to work with Unity's physics system.
-        /// </summary>
-        public bool HandPhysicsEnabled => handPhysicsEnabled;
-
-        [SerializeField]
-        [Tooltip("If set, hand colliders will be setup as triggers.")]
-        private bool useTriggers = false;
-
-        /// <summary>
-        /// If set, hand colliders will be setup as triggers.
-        /// </summary>
-        public bool UseTriggers => useTriggers;
-
-        [SerializeField]
-        [Tooltip("Set the bounds mode to use for calculating hand bounds.")]
-        private HandBoundsMode boundsMode = HandBoundsMode.Hand;
-
-        /// <summary>
-        /// Set the bounds mode to use for calculating hand bounds.
-        /// </summary>
-        public HandBoundsMode BoundsMode => boundsMode;
-
-        #endregion Global Hand Settings Overrides
     }
 }
