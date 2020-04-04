a72e989f9374ec3736788d189b0f239915d989de
Dino Fejzagic <dino.f@live.de>
2020-04-04 22:32:07 +0200

    Restore wmr hand profile and inspector

---
+++ XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Inspectors/WindowsMixedRealityHandControllerDataProviderProfileInspector.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
+using XRTK.Inspectors.Profiles;
+using XRTK.WindowsMixedReality.Profiles;
+
+namespace XRTK.WindowsMixedReality.Inspectors
+{
+    [CustomEditor(typeof(WindowsMixedRealityHandControllerDataProviderProfile))]
+    public class WindowsMixedRealityHandControllerDataProviderProfileInspector : BaseMixedRealityProfileInspector
+    {
+        // Global hand settings overrides
+        private SerializedProperty handMeshingEnabled;
+        private SerializedProperty handRayType;
+        private SerializedProperty handPhysicsEnabled;
+        private SerializedProperty useTriggers;
+        private SerializedProperty boundsMode;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            handMeshingEnabled = serializedObject.FindProperty(nameof(handMeshingEnabled));
+            handRayType = serializedObject.FindProperty(nameof(handRayType));
+            handPhysicsEnabled = serializedObject.FindProperty(nameof(handPhysicsEnabled));
+            useTriggers = serializedObject.FindProperty(nameof(useTriggers));
+            boundsMode = serializedObject.FindProperty(nameof(boundsMode));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            RenderHeader();
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Windows Mixed Reality Hand Controller Data Provider Settings", EditorStyles.boldLabel);
+            EditorGUILayout.HelpBox("This profile aids in configuring additional platform settings for the registered controller data provider.", MessageType.Info);
+
+            serializedObject.Update();
+
+            EditorGUILayout.Space();
+            EditorGUILayout.PropertyField(handMeshingEnabled);
+            EditorGUILayout.PropertyField(handRayType);
+            EditorGUILayout.PropertyField(handPhysicsEnabled);
+            EditorGUILayout.PropertyField(useTriggers);
+            EditorGUILayout.PropertyField(boundsMode);
+            EditorGUILayout.Space();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
