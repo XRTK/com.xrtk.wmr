98885222bb811074eed13a1c3be4a737f6564fa6
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-05 12:37:30 -0400

    fixed hand data provider inspector

--- XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Inspectors/WindowsMixedRealityHandControllerDataProviderProfileInspector.cs
+++ XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Inspectors/WindowsMixedRealityHandControllerDataProviderProfileInspector.cs
@@ -2,48 +2,22 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEditor;
-using XRTK.Inspectors.Profiles;
+using XRTK.Inspectors.Profiles.InputSystem.Controllers;
 using XRTK.WindowsMixedReality.Profiles;
 
 namespace XRTK.WindowsMixedReality.Inspectors
 {
     [CustomEditor(typeof(WindowsMixedRealityHandControllerDataProviderProfile))]
-    public class WindowsMixedRealityHandControllerDataProviderProfileInspector : BaseMixedRealityProfileInspector
+    public class WindowsMixedRealityHandControllerDataProviderProfileInspector : BaseMixedRealityHandControllerDataProviderProfileInspector
     {
-        // Global hand settings overrides
-        private SerializedProperty handMeshingEnabled;
-        private SerializedProperty handPhysicsEnabled;
-        private SerializedProperty useTriggers;
-        private SerializedProperty boundsMode;
-
-        protected override void OnEnable()
-        {
-            base.OnEnable();
-
-            handMeshingEnabled = serializedObject.FindProperty(nameof(handMeshingEnabled));
-            handPhysicsEnabled = serializedObject.FindProperty(nameof(handPhysicsEnabled));
-            useTriggers = serializedObject.FindProperty(nameof(useTriggers));
-            boundsMode = serializedObject.FindProperty(nameof(boundsMode));
-        }
-
         public override void OnInspectorGUI()
         {
             RenderHeader();
 
             EditorGUILayout.Space();
             EditorGUILayout.LabelField("Windows Mixed Reality Hand Controller Data Provider Settings", EditorStyles.boldLabel);
-            EditorGUILayout.HelpBox("This profile aids in configuring additional platform settings for the registered controller data provider.", MessageType.Info);
 
-            serializedObject.Update();
-
-            EditorGUILayout.Space();
-            EditorGUILayout.PropertyField(handMeshingEnabled);
-            EditorGUILayout.PropertyField(handPhysicsEnabled);
-            EditorGUILayout.PropertyField(useTriggers);
-            EditorGUILayout.PropertyField(boundsMode);
-            EditorGUILayout.Space();
-
-            serializedObject.ApplyModifiedProperties();
+            base.OnInspectorGUI();
         }
     }
 }
