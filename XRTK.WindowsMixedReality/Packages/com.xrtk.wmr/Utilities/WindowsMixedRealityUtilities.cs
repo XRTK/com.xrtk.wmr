be687a8a38a4c07bf108109baaafaf4ae832b67c
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-05 12:03:11 -0400

    less compiler symbols

--- XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Utilities/WindowsMixedRealityUtilities.cs
+++ XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Utilities/WindowsMixedRealityUtilities.cs
@@ -2,19 +2,18 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #if WINDOWS_UWP
+
 using System.Runtime.InteropServices;
 using UnityEngine.XR.WSA;
 using Windows.Perception.Spatial;
-#endif // WINDOWS_UWP
 
 namespace XRTK.WindowsMixedReality.Utilities
 {
     public static class WindowsMixedRealityUtilities
     {
-#if WINDOWS_UWP
         private static SpatialCoordinateSystem spatialCoordinateSystem = null;
 
         public static SpatialCoordinateSystem SpatialCoordinateSystem => spatialCoordinateSystem ?? (spatialCoordinateSystem = Marshal.GetObjectForIUnknown(WorldManager.GetNativeISpatialCoordinateSystemPtr()) as SpatialCoordinateSystem);
-#endif // WINDOWS_UWP
     }
 }
+#endif // WINDOWS_UWP
