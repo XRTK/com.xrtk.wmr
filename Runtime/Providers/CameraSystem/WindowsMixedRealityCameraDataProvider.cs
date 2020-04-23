fb9afd575a74cbc5bada49f0ba7dc9bf8ec97ee5
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-23 18:18:36 -0400

    Camera System Refactor (#66)
    * updated reference
    
    * added WMR Camera Data Provider
    
    * updated WMR Camera Data Provider
    
    * updated camera rig reference
    
    * updated icon
    
    * moved WMR Camera data provider into correct file location
    
    * updated data provider constructors
    
    * added service parent reference
    
    * Added rig reset override
    
    * reverted changes
    
    * removed unused reference
    
    * added camera profile

---
+++ Runtime/Providers/CameraSystem/WindowsMixedRealityCameraDataProvider.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Definitions.CameraSystem;
+using XRTK.Interfaces.CameraSystem;
+using XRTK.Providers.CameraSystem;
+
+namespace XRTK.WindowsMixedReality.Providers.CameraSystem
+{
+    public class WindowsMixedRealityCameraDataProvider : BaseCameraDataProvider
+    {
+        /// <inheritdoc />
+        public WindowsMixedRealityCameraDataProvider(string name, uint priority, BaseMixedRealityCameraDataProviderProfile profile, IMixedRealityCameraSystem parentService)
+            : base(name, priority, profile, parentService)
+        {
+        }
+
+        /// <inheritdoc />
+        public override bool IsOpaque
+        {
+            get
+            {
+#if UNITY_WSA
+                return UnityEngine.XR.WSA.HolographicSettings.IsDisplayOpaque;
+#else
+                return base.IsOpaque;
+#endif
+            }
+        }
+    }
+}
