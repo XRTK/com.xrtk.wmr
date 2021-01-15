78dcfc6d3370336de5ded1731dca25cb4be8c3f4
Dino Fejzagić <dino.f@live.de>
2021-01-15 04:07:59 +0100

    Fix editor utilities namespace (#115)
    * Fix editor utilities namespace
    
    * Update package.json
    
    * updated packages-lock.json
    
    Co-authored-by: Stephen Hodgson <hodgson.designs@gmail.com>

--- Editor/WindowsMixedRealityPathFinder.cs
+++ Editor/WindowsMixedRealityPathFinder.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEngine;
-using XRTK.Utilities.Editor;
+using XRTK.Editor.Utilities;
 
 namespace XRTK.WindowsMixedReality.Editor
 {
