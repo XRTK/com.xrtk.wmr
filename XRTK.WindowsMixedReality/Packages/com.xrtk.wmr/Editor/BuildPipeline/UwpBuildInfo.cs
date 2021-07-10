3c8d60e120d49de3996802c9c1a1b17c9c7c7083
Stephen Hodgson <hodgson.designs@gmail.com>
2021-07-10 13:19:48 -0400

    a bit more polsih to the build window

--- XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Editor/BuildPipeline/UwpBuildInfo.cs
+++ XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Editor/BuildPipeline/UwpBuildInfo.cs
@@ -15,6 +15,34 @@ namespace XRTK.Editor.BuildPipeline
     [RuntimePlatform(typeof(UniversalWindowsPlatform))]
     public class UwpBuildInfo : BuildInfo
     {
+        public enum Platform
+        {
+            x86 = 0,
+            x64 = 1,
+            ARM = 2,
+            ARM64 = 3,
+        }
+
+        public enum VerbosityLevel
+        {
+            Quiet = 0,
+            Minimal,
+            Normal,
+            Detailed,
+            Diagnostic
+        }
+
+        /// <inheritdoc />
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            if (string.IsNullOrWhiteSpace(solutionName))
+            {
+                solutionName = $"{PlayerSettings.productName}\\{PlayerSettings.productName}.sln";
+            }
+        }
+
         /// <inheritdoc />
         public override BuildTarget BuildTarget => BuildTarget.WSAPlayer;
 
@@ -35,23 +63,13 @@ namespace XRTK.Editor.BuildPipeline
             set => version = value;
         }
 
+        [SerializeField]
         private string solutionName;
 
         /// <summary>
         /// The name of the Visual Studio .sln file generated.
         /// </summary>
-        public string SolutionName
-        {
-            get
-            {
-                if (string.IsNullOrWhiteSpace(solutionName))
-                {
-                    solutionName = $"{PlayerSettings.productName}\\{PlayerSettings.productName}.sln";
-                }
-
-                return solutionName;
-            }
-        }
+        public string SolutionName => solutionName;
 
         [SerializeField]
         private bool buildAppx;
@@ -77,6 +95,27 @@ namespace XRTK.Editor.BuildPipeline
             set => rebuildAppx = value;
         }
 
+        [SerializeField]
+        private Platform platformArchitecture = Platform.ARM64;
+
+        /// <summary>
+        /// The build platform architecture (i.e. x86, x64, ARM, ARM64)
+        /// </summary>
+        public Platform PlatformArchitecture
+        {
+            get => platformArchitecture;
+            set => platformArchitecture = value;
+        }
+
+        [SerializeField]
+        private VerbosityLevel verbosity = VerbosityLevel.Quiet;
+
+        public VerbosityLevel Verbosity
+        {
+            get => verbosity;
+            set => verbosity = value;
+        }
+
         public string UwpSdk => EditorUserBuildSettings.wsaUWPSDK;
 
         public string MinSdk => EditorUserBuildSettings.wsaMinUWPSDK;
@@ -89,6 +128,23 @@ namespace XRTK.Editor.BuildPipeline
         {
             var values = (PlayerSettings.WSATargetFamily[])Enum.GetValues(typeof(PlayerSettings.WSATargetFamily));
             return values.Where(PlayerSettings.WSA.GetTargetDeviceFamily).ToArray();
+        }
+
+        /// <inheritdoc />
+        public override void OnPreProcessBuild(BuildReport report)
+        {
+            if (!MixedRealityToolkit.ActivePlatforms.Contains(BuildPlatform) ||
+                EditorUserBuildSettings.activeBuildTarget != BuildTarget)
+            {
+                return;
+            }
+
+            EditorUserBuildSettings.wsaArchitecture = platformArchitecture.ToString();
+
+            if (!UwpAppxBuildTools.ValidateUwpSdk())
+            {
+                throw new ArgumentException("Invalid Windows SDK");
+            }
         }
 
         /// <inheritdoc />
@@ -129,11 +185,65 @@ namespace XRTK.Editor.BuildPipeline
             {
                 switch (arguments[i])
                 {
+                    case "-verbosity":
+                        var verb = arguments[++i].Substring(1);
+
+                        switch (verb)
+                        {
+                            case "q":
+                            case "quiet":
+                                Verbosity = VerbosityLevel.Quiet;
+                                break;
+                            case "m":
+                            case "minimal":
+                                Verbosity = VerbosityLevel.Minimal;
+                                break;
+                            case "n":
+                            case "normal":
+                                Verbosity = VerbosityLevel.Normal;
+                                break;
+                            case "d":
+                            case "detailed":
+                                Verbosity = VerbosityLevel.Detailed;
+                                break;
+                            case "diag":
+                            case "diagnostic":
+                                Verbosity = VerbosityLevel.Diagnostic;
+                                break;
+                            default:
+                                Debug.LogError($"Failed to parse -{nameof(verbosity)}: \"{verb}\"");
+                                break;
+                        }
+
+                        break;
                     case "-buildAppx":
                         BuildAppx = true;
                         break;
                     case "-rebuildAppx":
                         RebuildAppx = true;
+                        break;
+                    case "-buildArchitecture":
+                        var architecture = arguments[++i].Substring(1);
+
+                        switch (architecture)
+                        {
+                            case "x86":
+                                PlatformArchitecture = Platform.x86;
+                                break;
+                            case "x64":
+                                PlatformArchitecture = Platform.x64;
+                                break;
+                            case "ARM":
+                                PlatformArchitecture = Platform.ARM;
+                                break;
+                            case "ARM64":
+                                PlatformArchitecture = Platform.ARM64;
+                                break;
+                            default:
+                                Debug.LogError($"Failed to parse -buildArchitecture: \"{architecture}\"");
+                                break;
+                        }
+
                         break;
                 }
             }
--- XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Editor/BuildPipeline/UwpBuildInfoInspector.cs
+++ XRTK.WindowsMixedReality/Packages/com.xrtk.wmr/Editor/BuildPipeline/UwpBuildInfoInspector.cs
@@ -1,43 +1,29 @@
 // Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 namespace XRTK.Editor.BuildPipeline
 {
     [CustomEditor(typeof(UwpBuildInfo))]
     public class UwpBuildInfoInspector : BuildInfoInspector
     {
-        private enum Architecture
-        {
-            x86 = 0,
-            x64 = 1,
-            ARM = 2,
-            ARM64 = 3,
-        }
-
         private SerializedProperty buildAppx;
         private SerializedProperty rebuildAppx;
-        private List<Version> uwpSdkVersions = new List<Version>();
-
-        private static bool IsValidSdkInstalled { get; set; } = true;
+        private SerializedProperty platformArchitecture;
+        private SerializedProperty verbosity;
 
         protected override void OnEnable()
         {
             base.OnEnable();
 
-#if UNITY_EDITOR_WIN
-            LoadUwpSdkPaths();
-#endif
-
             buildAppx = serializedObject.FindProperty(nameof(buildAppx));
             rebuildAppx = serializedObject.FindProperty(nameof(rebuildAppx));
+            platformArchitecture = serializedObject.FindProperty(nameof(platformArchitecture));
+            verbosity = serializedObject.FindProperty(nameof(verbosity));
         }
 
         public override void OnInspectorGUI()
@@ -80,40 +66,11 @@ namespace XRTK.Editor.BuildPipeline
                 buildInfo.Configuration = buildConfigString;
             }
 
-            // Build Platform (and save setting, if it's changed)
-            var currentArchitectureString = EditorUserBuildSettings.wsaArchitecture;
-
-            var buildArchitecture = Architecture.x86;
-
-            if (currentArchitectureString.ToLower().Equals("x86"))
-            {
-                buildArchitecture = Architecture.x86;
-            }
-            else if (currentArchitectureString.ToLower().Equals("x64"))
-            {
-                buildArchitecture = Architecture.x64;
-            }
-            else if (currentArchitectureString.ToLower().Equals("arm"))
-            {
-                buildArchitecture = Architecture.ARM;
-            }
-            else if (currentArchitectureString.ToLower().Equals("arm64"))
-            {
-                buildArchitecture = Architecture.ARM64;
-            }
-
-            buildArchitecture = (Architecture)EditorGUILayout.EnumPopup("Build Platform", buildArchitecture);
-
-            string newBuildArchitectureString = buildArchitecture.ToString();
-
-            if (newBuildArchitectureString != currentArchitectureString)
-            {
-                EditorUserBuildSettings.wsaArchitecture = newBuildArchitectureString;
-            }
-
+            EditorGUILayout.PropertyField(platformArchitecture);
+            EditorGUILayout.PropertyField(verbosity);
             EditorGUILayout.EndHorizontal();
 
-            ValidateUwpSdk();
+            UwpAppxBuildTools.ValidateUwpSdk(true);
 
             if (GUILayout.Button("Open in Visual Studio"))
             {
@@ -161,62 +118,6 @@ namespace XRTK.Editor.BuildPipeline
             }
 
             serializedObject.ApplyModifiedProperties();
-        }
-
-        private void LoadUwpSdkPaths()
-        {
-            var windowsSdkPaths = Directory.GetDirectories(@"C:\Program Files (x86)\Windows Kits\10\Lib");
-
-            foreach (var path in windowsSdkPaths)
-            {
-                uwpSdkVersions.Add(new Version(path.Substring(path.LastIndexOf(@"\", StringComparison.Ordinal) + 1)));
-            }
-
-            // There is no well-defined enumeration of Directory.GetDirectories, so the list
-            // is sorted prior to use later in this class.
-            uwpSdkVersions.Sort();
-        }
-
-        private void ValidateUwpSdk()
-        {
-            // SDK and MS Build Version (and save setting, if it's changed)
-            string currentSDKVersion = EditorUserBuildSettings.wsaMinUWPSDK;
-
-            Version chosenSDKVersion = null;
-
-            for (var i = 0; i < uwpSdkVersions.Count; i++)
-            {
-                // windowsSdkVersions is sorted in ascending order, so we always take
-                // the highest SDK version that is above our minimum.
-                if (uwpSdkVersions[i] >= UwpBuildDeployPreferences.MIN_SDK_VERSION)
-                {
-                    chosenSDKVersion = uwpSdkVersions[i];
-                }
-            }
-
-            EditorGUILayout.HelpBox($"Minimum Required SDK Version: {currentSDKVersion}", MessageType.Info);
-
-            // Throw exception if user has no Windows 10 SDK installed
-            if (chosenSDKVersion == null)
-            {
-                if (IsValidSdkInstalled)
-                {
-                    Debug.LogError($"Unable to find the required Windows 10 SDK Target!\nPlease be sure to install the {UwpBuildDeployPreferences.MIN_SDK_VERSION} SDK from Visual Studio Installer.");
-                }
-
-                EditorGUILayout.HelpBox($"Unable to find the required Windows 10 SDK Target!\nPlease be sure to install the {UwpBuildDeployPreferences.MIN_SDK_VERSION} SDK from Visual Studio Installer.", MessageType.Error);
-                GUILayout.EndVertical();
-                IsValidSdkInstalled = false;
-                return;
-            }
-
-            IsValidSdkInstalled = true;
-            var newSdkVersion = chosenSDKVersion.ToString();
-
-            if (!newSdkVersion.Equals(currentSDKVersion))
-            {
-                EditorUserBuildSettings.wsaMinUWPSDK = newSdkVersion;
-            }
         }
     }
 }
